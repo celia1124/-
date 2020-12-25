876476c6f8f381dc916e2c6dc332294a37f3662a
Celia <peipeitzu0209@gmail.com>
2020-12-25 12:40:28 +0800

    demo

--- ab-edit.php
+++ ab-edit.php
@@ -1,17 +1,16 @@
-
 <?php
-require __DIR__. '/is_admin.php';
-require __DIR__. '/db_connect.php';
+require __DIR__ . '/is_admin.php';
+require __DIR__ . '/db_connect.php';
 
-if(! isset($_GET['sid'])){
+if (!isset($_GET['sid'])) {
     header('Location: db_list.php');
     exit;
 }
 $sid = intval($_GET['sid']);
 
 $row = $pdo
-->query("SELECT * FROM custo_order WHERE sid=$sid")
-->fetch();
+    ->query("SELECT * FROM custo_order WHERE sid=$sid")
+    ->fetch();
 
 // if(empty($row)){
 //     header('Location: db_list.php');
@@ -19,84 +18,94 @@ $row = $pdo
 // }
 
 ?>
-<?php include __DIR__. '/parts/head.php';?>
-<?php include __DIR__. '/parts/navbar.php';?>
+<?php include __DIR__ . '/parts/head.php'; ?>
+<?php include __DIR__ . '/parts/navbar.php'; ?>
 
 
 <style>
+    #error-msg {
 
+        color: red;
+        border-color: red;
+    }
 
-#error-msg{
+    .card {
+        margin-top: 20px;
+    }
 
-    color:red;
-    border-color:red;
-}
+    body {
+        background-color: #F7D7F7;
+    }
 
+    .card {
+        background-color: #B441D9;
+        color: white;
+        border-radius: 40px;
+    }
 
-
+    .btn-primary {
+        color: #fff;
+        background-color: #F563C4;
+        border-color: #F563C4;
+        border-radius: 60px;
+    }
 </style>
 
 <div class="container">
     <div class="row d-flex justify-content-center">
         <div class="col-lg-6">
-           
+
             <div class="alert alert-danger" role="alert" id="info" style="display:none">
-              錯誤
+                錯誤
             </div>
-           
-            
-            <div class="card" >
+
+
+            <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">編輯客製化</h5>
 
                     <form name="form1" novalidate onsubmit="checkForm(); return false;">
-                    <input type="hidden" name="sid" value="<?= $sid ?>">
-                    <div class="form-group">
+                        <input type="hidden" name="sid" value="<?= $sid ?>">
+                        <div class="form-group">
                             <label for="name">** 品名</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="客製化**蛋糕"
-                            value="<?= htmlentities($row['name']) ?>">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="客製化**蛋糕" value="<?= htmlentities($row['name']) ?>">
                             <small class="form-text" id="error-msg" style="display : none">0000</small>
                         </div>
                         <div class="form-group">
                             <label for="text">配料-香蕉</label>
-                            <input type="text" class="form-control" id="banana" name="banana" placeholder="yes / no"
-                            value="<?= htmlentities($row['banana']) ?>">
-                            
-                            
+                            <input type="text" class="form-control" id="banana" name="banana" placeholder="yes / no" value="<?= htmlentities($row['banana']) ?>">
+
+
                         </div>
                         <div class="form-group">
                             <label for="text">配料-草莓</label>
-                            <input type="text" class="form-control" id="straw" name="straw" placeholder="yes / no" >
-                            
+                            <input type="text" class="form-control" id="straw" name="straw" placeholder="yes / no">
+
                         </div>
                         <div class="form-group">
                             <label for="berry">配料-藍莓</label>
-                            <input type="text" class="form-control" id="berry" name="berry" placeholder="yes / no"
-                            value="<?= htmlentities($row['blueberry']) ?>">
-                            
+                            <input type="text" class="form-control" id="berry" name="berry" placeholder="yes / no" value="<?= htmlentities($row['blueberry']) ?>">
+
                         </div>
                         <div class="form-group">
                             <label for="text">產品編號</label>
-                            <input type="text" class="form-control" id="proid" name="proid" placeholder="ex:A011"
-                            value="<?= htmlentities($row['productid']) ?>">
+                            <input type="text" class="form-control" id="proid" name="proid" placeholder="ex:A011" value="<?= htmlentities($row['productid']) ?>">
                             <small class="form-text" id="error-msg" style="display : none">0000</small>
                         </div>
 
                         <div class="form-group">
                             <label for="text">銷售編號</label>
-                            <input type="text" class="form-control" id="orderid" name="orderid"
-                             pattern="^[A-Z]\d{3}?#\d{0,}$"  placeholder="ex:A011#1"
-                             value="<?= htmlentities($row['saleid']) ?>">
-                             <small class="form-text" id="error-msg" style="display : none">0000</small>
+                            <input type="text" class="form-control" id="orderid" name="orderid" pattern="^[A-Z]\d{3}?#\d{0,}$" placeholder="ex:A011#1" value="<?= htmlentities($row['saleid']) ?>">
+                            <small class="form-text" id="error-msg" style="display : none">0000</small>
                         </div>
-                        
+
                         <button type="submit" class="btn btn-primary">修改</button>
                     </form>
 
 
                 </div>
             </div>
-            
+
         </div>
     </div>
 
@@ -104,29 +113,29 @@ $row = $pdo
 
 </div>
 
-<?php include __DIR__. '/parts/scripts.php';?>
+<?php include __DIR__ . '/parts/scripts.php'; ?>
 
 
 
 <script>
     const info = document.querySelector('#info');
 
-   const name = document.querySelector('#name');
-   const proid = document.querySelector('#proid');
-   const orderid = document.querySelector('#orderid');
-   const proid_re =/^[A-Z]\d{3}/;
-   const orderid_re =/^[A-Z]\d{3}?#\d{0,}$/;
+    const name = document.querySelector('#name');
+    const proid = document.querySelector('#proid');
+    const orderid = document.querySelector('#orderid');
+    const proid_re = /^[A-Z]\d{3}/;
+    const orderid_re = /^[A-Z]\d{3}?#\d{0,}$/;
 
 
 
-   
-    
-    function checkForm(){
+
+
+    function checkForm() {
         info.style.display = 'none';
 
         let isPass = true;
 
-        
+
 
 
         name.style.borderColor = '#6CFF33';
@@ -137,8 +146,8 @@ $row = $pdo
 
         orderid.style.borderColor = '#6CFF33';
         orderid.nextElementSibling.style.display = 'none';
-        
-        if(name.value.length <5){
+
+        if (name.value.length < 5) {
             isPass = false;
             name.style.borderColor = 'red';
             let small = name.closest('.form-group').querySelector('small');
@@ -146,8 +155,8 @@ $row = $pdo
             small.style.display = 'block';
         }
 
-       
-        if(!proid_re.test(proid.value) ){
+
+        if (!proid_re.test(proid.value)) {
             isPass = false;
             proid.style.borderColor = 'red';
             let small = proid.closest('.form-group').querySelector('small');
@@ -155,7 +164,7 @@ $row = $pdo
             small.style.display = 'block';
         }
 
-        if(!orderid_re.test(orderid.value)){
+        if (!orderid_re.test(orderid.value)) {
             isPass = false;
             orderid.style.borderColor = 'red';
             let small = orderid.closest('.form-group').querySelector('small');
@@ -164,45 +173,39 @@ $row = $pdo
         }
 
 
-        
-        if(isPass){
+
+        if (isPass) {
             const fd = new FormData(document.form1);
 
             fetch('ab-edit-api.php', {
-                method: 'POST',
-                body: fd
-            })
-            .then(r=>r.json())
-            .then(obj=>{
-                console.log(obj);
-                if(obj.success)
-                {
-                    // 新增成功
-                    info.classList.remove('alert-danger');
-                    info.classList.add('alert-success');
-                    info.innerHTML = '修改成功';
-                } 
-                 else
-                {
-                    info.classList.remove('alert-success');
-                    info.classList.add('alert-danger');
-                    info.innerHTML = obj.error || '修改失敗' ;
-                }
-                info.style.display = 'block';
-            });
-            
+                    method: 'POST',
+                    body: fd
+                })
+                .then(r => r.json())
+                .then(obj => {
+                    console.log(obj);
+                    if (obj.success) {
+                        // 新增成功
+                        info.classList.remove('alert-danger');
+                        info.classList.add('alert-success');
+                        info.innerHTML = '修改成功';
+                    } else {
+                        info.classList.remove('alert-success');
+                        info.classList.add('alert-danger');
+                        info.innerHTML = obj.error || '修改失敗';
+                    }
+                    info.style.display = 'block';
+                });
+
         }
 
-        
-        
+
+
 
 
     }
-
-
-
 </script>
 
 
 
-<?php include __DIR__. '/parts/foot.php';?>
+<?php include __DIR__ . '/parts/foot.php'; ?>
--- parts/head.php
+++ parts/head.php
@@ -1,17 +1,19 @@
 <?php
-define('WEB_ROOT', '/mfee-proj - 複製/');
+define('WEB_ROOT', '/MFEE-PROJ-複製/MYSQL/');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=isset($title) ? $title."-" :" " ?>客製化</title>
-    <link rel="stylesheet" href="<?=WEB_ROOT?>./fontawesome/css/all.css">
-    <link rel="stylesheet" href="<?=WEB_ROOT?>./bootstrap-4.5.3-dist/css/bootstrap.css">
-    
-    
+    <title><?= isset($title) ? $title . "-" : " " ?>客製化</title>
+    <link rel="stylesheet" href="<?= WEB_ROOT ?>fontawesome/css/all.css">
+    <link rel="stylesheet" href="<?= WEB_ROOT ?>bootstrap-4.5.3-dist/css/bootstrap.css">
+
+
 
 </head>
+
 <body>
